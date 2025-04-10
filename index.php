0482834111ca1ad1d0db89b968f4d132c6f3b93f
guilherme070409 <guigatomoura@gmail.com>
2025-04-10 10:14:41 -0300

    145

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
             <button type="submit" class="btn btn-primary btn-block mb-4">Logar</button>
       
             <div class="text-center">
-              <p>Não cadastrado? <a href="cadastro/cadastro.html">cadastro</a></p>
+              <p>Não cadastrado? <a href="cadastro/cadastro.php">cadastro</a></p>
             </div>
       </form>
   </div>
