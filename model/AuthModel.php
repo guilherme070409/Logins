fcf56000571af271ce0a9889e11c3959910c97ab
guilherme070409 <guigatomoura@gmail.com>
2025-04-30 11:19:41 -0300

    upa

--- model/AuthModel.php
+++ model/AuthModel.php
@@ -1,25 +1,35 @@
 <?php
-public function login($email, $senha) {
-    $sql = "SELECT u.*, p.Nome 
-            FROM usuario u
-            JOIN pessoa p ON u.PESSOA_ID = p.ID
-            WHERE u.`E-MAIL` = ?";
-    
-    $stmt = $this->conn->prepare($sql);
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
+require_once '../service/conexao.php';
 
-    if ($result->num_rows === 1) {
-        $usuario = $result->fetch_assoc();
-        if (password_verify($senha, $usuario['SENHA'])) {
-            return [
-                'id' => $usuario['ID'],
-                'nome' => $usuario['Nome'],
-                'email' => $usuario['E-MAIL']
-            ];
-        }
+class AuthModel {
+    private $conn;
+
+    public function __construct($conexao) {
+        $this->conn = $conexao;
     }
-    throw new Exception("Credenciais inválidas!");
+
+    public function login($email, $senha) {
+        $sql = "SELECT u.*, p.Nome 
+                FROM usuario u
+                JOIN pessoa p ON u.PESSOA_ID = p.ID
+                WHERE u.`E-MAIL` = ?";
+        
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows === 1) {
+            $usuario = $result->fetch_assoc();
+            if (password_verify($senha, $usuario['SENHA'])) {
+                return [
+                    'id' => $usuario['ID'],
+                    'nome' => $usuario['Nome'],
+                    'email' => $usuario['E-MAIL']
+                ];
+            }
+        }
+        throw new Exception("Credenciais inválidas!");
+    }
 }
 ?>
